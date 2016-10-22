d5c84141bf0995d3270b59a64b814359b9b83cd0
Dani Pletter <dpletter@users.noreply.github.com>
2016-10-22 16:50:55 -0700

    add docs, server restarts and backgrounds

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 
+touch .pid
+kill -9 `cat .pid`
+open http://localhost:8000/docs/
 open http://localhost:8000/examples/
 open http://localhost:8000/index.htm
-python -m SimpleHTTPServer
+python -m SimpleHTTPServer &
+echo $! > .pid
+
